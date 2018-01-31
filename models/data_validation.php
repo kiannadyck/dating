476c0adb5957445f221ec8c4cbd1e54f3b3d0c40
Kianna Dyck <kdyck@mail.greenriver.edu>
2018-01-30 23:17:38 -0500

    added form pages and profile summary in pages folder. added a validation file in new models folder.

---
+++ models/data_validation.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Function checks to see that a string is all alphabetic
+ * @param $name Name entered in the form by the client.
+ */
+function validName($name)
+{
+
+}
+
+/**
+ * Function checks to see that an age is numeric and over 18.
+ * @param $age Age entered in the form by the client.
+ */
+function validAge($age)
+{
+
+}
+
+/**
+ * Function checks to see that a phone number is valid.
+ * @param $phone Phone Number entered in the form by the client.
+ */
+function validPhone($phone)
+{
+
+}
+
+/**
+ * Function checks each selected outdoor interest against a list of valid options.
+ * @param $interests Outdoor interests selected by client
+ */
+function validOutdoor($interests)
+{
+
+}
+
+/**
+ * Function checks each selected indoor interest against a list of valid options.
+ * @param $interests Indoor interests selected by the client.
+ */
+function validIndoor($interests)
+{
+
+}
